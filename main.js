3c1e5ab3fdbb880a3dd76682852f700ec0b96ffb
Sudharshan <sudhar393@gmail.com>
2018-03-31 16:39:55 +0800

    basic boilerplate with bootstrap and autoreload enabled

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+"use strict"
+
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
 const {app, BrowserWindow} = electron;
+
+let MainWindow;
+
+// Enable autoreloading
+require('electron-reload')(__dirname);
+
+// Wait for app to be ready
+app.on('ready', function(){
+    MainWindow = new BrowserWindow({});
+    // Load HTML file
+    MainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
