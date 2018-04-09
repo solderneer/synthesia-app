94ae4c69d77270ff65ff2dd55c492ca80bf627b8
Sudharshan <sudhar393@gmail.com>
2018-04-09 08:13:43 +0800

    added toggle function

--- js/app.js
+++ js/app.js
@@ -5,12 +5,34 @@ const electron = require('electron')
 
 const {remote, ipcRenderer} = electron;
 
-console.log('The author of this app is Sudharshan');
+console.log('The author of this app is Sudharshan'); // Testing
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log(document.querySelector('#hello').innerHTML); // Testing
 
     document.querySelector('#connect').onclick = () => {
-        ipcRenderer.sendSync('exit', null);
+        //ipcRenderer.sendSync('exit', null);
+        document.querySelector('#initial').style.display = 'none';
+        document.querySelector('#settings').style.display = 'block';
+
+        let pitchEn = document.querySelector('#pitchen');
+        let instEn = document.querySelector('#insten');
+
+        pitchEn.onclick = () => {
+            if(pitchEn.checked == true) {
+                instEn.checked = false;
+            }
+        };
+
+        instEn.onclick = () => {
+            if(instEn.checked == true) {
+                pitchEn.checked = false;
+            }
+        };
+    };
+
+    document.querySelector('#disconnect').onclick = () => {
+        document.querySelector('#initial').style.display = 'block';
+        document.querySelector('#settings').style.display = 'none';
     };
 });
